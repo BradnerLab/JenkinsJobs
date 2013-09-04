aff5ffb9c74ea8aebed9f2f965816946ac82d7a7
John DiMatteo <jdimatteo@gmail.com>
2013-09-04 08:47:41 -0400

    Updated to allow multiple selections

--- Bam_Plot/bamPlotArgumentsFromTable.py
+++ Bam_Plot/bamPlotArgumentsFromTable.py
@@ -4,18 +4,23 @@ import csv
 import sys
 
 if len(sys.argv) != 3:
-    print "Usage: %s <table_data_file> <name_selection_from_data_file>" % sys.argv[0]
+    print "Usage: %s [table data file] [name selection(s) from data file]" % sys.argv[0]
     print ""
     print "Returns the corresponding bam file and genome in the following format: BAM_FILE -g GENOME" 
     print ""
-    print "Example:"
+    print "Examples:"
     print ""
-    print '\t$ %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt MM1S_BRD4_500nM_JQ1' % sys.argv[0]
+    print '\t$ python %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt MM1S_BRD4_500nM_JQ1' % sys.argv[0]
     print "\t/ifs/labs/bradner/bam/hg18/mm1s/05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam -g HG18" 
+    print "\t$"
+    print ""
+    print '\t$ python %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt MM1S_BRD4_500nM_JQ1,LY1_BRD4_DMSO' % sys.argv[0]
+    print "\t/ifs/labs/bradner/bam/hg18/mm1s/05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam,/ifs/labs/bradner/bam/hg18/ly1/06152012_C0VD4ACXX_6.AGTTCC.hg18.bwt.sorted.bam -g HG18" 
     print "\t$"
     sys.exit(-1)
 
-(table_data_file_name, name) = (sys.argv[1], sys.argv[2])
+(table_data_file_name, comma_seperated_names) = (sys.argv[1], sys.argv[2])
+names = comma_seperated_names.split(",")
 
 # files are assumed to be in a 5 column tab delimited format such as the following:
 # GENOME  SOURCE  CELL_TYPE       NAME    BAMFILE
@@ -35,13 +40,23 @@ file_col    = 4
 # genome_col = 3
 # color_col  = 4
 
+files  = ""
+genome = ""
+
 with open(table_data_file_name, 'r') as table_data_file:
     reader = csv.reader(table_data_file, delimiter='\t')
    
     for record in reader:
-        if record[name_col] == name:
-            print "%s -g %s" % (record[file_col], record[genome_col]) 
-            sys.exit(0)
+        if record[name_col] in names:
+            if len(files) > 0:
+                files += ","
+            files += record[file_col]
+            genome = record[genome_col]
 
-print "Record not found for name '%s'" % name
-sys.exit(1)
+
+if files == "":
+    print "Record not found for name '%s' in %s" % (name, table_data_file_name) 
+    sys.exit(1)
+else:
+    print "%s -g %s" % (files, genome) 
+    
