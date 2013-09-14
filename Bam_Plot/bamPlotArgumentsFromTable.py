b730ba39e8bdf069c3187a491742f1559900dedb
John DiMatteo <jdimatteo@gmail.com>
2013-09-13 23:50:29 -0400

    Updated script to work with multi-level multi select plugin

--- Bam_Plot/bamPlotArgumentsFromTable.py
+++ Bam_Plot/bamPlotArgumentsFromTable.py
@@ -4,23 +4,30 @@ import csv
 import sys
 
 if len(sys.argv) != 3:
-    print "Usage: %s [table data file] [name selection(s) from data file]" % sys.argv[0]
+    print "Usage: %s [table data file] [selection(s) from data file]" % sys.argv[0]
     print ""
     print "Returns the corresponding bam file and genome in the following format: BAM_FILE -g GENOME" 
     print ""
+    print "Note that the selection(s) format is from the Extended Choice (Multi-Level Multi Select) Jenkins plugin."
+    print ""
     print "Examples:"
     print ""
-    print '\t$ python %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt MM1S_BRD4_500nM_JQ1' % sys.argv[0]
+    print '\t$ python %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt "HG18,Multiple Myeloma,MM1S,MM1S_BRD4_500nM_JQ1"' % sys.argv[0]
     print "\t/ifs/labs/bradner/bam/hg18/mm1s/05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam -g HG18" 
     print "\t$"
     print ""
-    print '\t$ python %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt MM1S_BRD4_500nM_JQ1,LY1_BRD4_DMSO' % sys.argv[0]
+    print '\t$ python %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt "HG18,Multiple Myeloma,MM1S,MM1S_BRD4_500nM_JQ1,,HG18,Diffuse large B-cell lymphoma,LY1,LY1_BCL6_DMSO"' % sys.argv[0]
     print "\t/ifs/labs/bradner/bam/hg18/mm1s/05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam,/ifs/labs/bradner/bam/hg18/ly1/06152012_C0VD4ACXX_6.AGTTCC.hg18.bwt.sorted.bam -g HG18" 
     print "\t$"
     sys.exit(-1)
 
-(table_data_file_name, comma_seperated_names) = (sys.argv[1], sys.argv[2])
-names = comma_seperated_names.split(",")
+(table_data_file_name, comma_seperated_selections) = (sys.argv[1], sys.argv[2])
+
+names = []
+for selection in comma_seperated_selections.split(',,'):
+    selection_columns = selection.split(',')
+    if len(selection_columns) == 4:
+        names.append(selection_columns[3]) 
 
 # files are assumed to be in a 5 column tab delimited format such as the following:
 # GENOME  SOURCE  CELL_TYPE       NAME    BAMFILE
